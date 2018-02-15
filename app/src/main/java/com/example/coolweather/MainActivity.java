2882c7460aee05bfefa36df3aa60584a63d40892
sehor <714534524@qq.com>
2018-02-15 21:33:39 +0800

    First Commit.

---
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String HeFengRegKey="93a755669ce74c049e6c07fec77734d3";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
